577dc1214d185148be253603e848b480f25360ea
Benedict Baah <82664842+BenedictBen@users.noreply.github.com>
2023-08-29 04:39:42 +0000

    Update Button.js

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Button = (props) => {
+const Button = ({label}) => {
   return (
     <div className='btn'>
     
-    <button>{props.name}</button>
+    <button>{label}</button>
    
 
     </div>
